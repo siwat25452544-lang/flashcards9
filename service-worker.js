d7bf8a90198d1499533cad874feb79e462d2eda9
siwat25452544-lang <siwat25452544@gmail.com>
2025-08-11 05:24:43 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// v10
-const CACHE='en-trainer-v10';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=10','./manifest.json','./service-worker.js?v=10'])))});
+// v11
+const CACHE='en-trainer-v11';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=11','./manifest.json','./service-worker.js?v=11'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))})
