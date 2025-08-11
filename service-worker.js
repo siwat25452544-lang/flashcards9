fcf97702401e08df855f4c091ebb705bc310328b
siwat25452544-lang <siwat25452544@gmail.com>
2025-08-11 23:00:59 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,38 @@
-// v11
-const CACHE='en-trainer-v11';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=11','./manifest.json','./service-worker.js?v=11'])))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))})
+const CACHE_NAME = 'flashcards-cache-v2';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
+  './assets/icon-1024.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => {
+      return cached || fetch(req).then(res => {
+        if (req.method === 'GET') {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
